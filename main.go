ad6264db1928d72eac38cf181ffe2b3baf70db7f
iamtonmoy0 <tonmoy2code@gmail.com>
2023-01-09 20:30:30 +0600

    mx record added

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("domain,hasMX,hasSPF,spfRecord,hasDMARC,dmarcRecord\n")
+	for scanner.Scan(){
+		checkDomain(scanner.Text())
+	}
+	if err := scanner.Err(),err!=nil{
+		log.Fatal("Error: could not read from input: %v\n",err)
+	}
+}
+
+func checkDomain(domain string) {
+var hasMX,hasSPF,hasDMARC bool
+var spfRecord, dmarcRecord string
+
+mxRecords , err := net.LookupMX(domain)
+if err!= nil{
+	log.Printf("Error: %v\n",err)
+}
+if len(mxRecords)> 0 {
+	hasMX = true
+}
+}
